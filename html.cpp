a4ceda479127e85cf1520dbc85a2363d9c1acd04
Charles <c650@users.noreply.github.com>
2017-06-09 20:56:57 -0400

    start implementation

--- html.cpp
+++ html.cpp
@@ -3,3 +3,30 @@
 #include <vector>
 #include <unordered_map>
 #include <stdexcept>
+
+namespace hpbeo = HTMLParser::BaseElementObject;
+
+hpbeo::BaseElementObject();
+hpbeo::~BaseElementObject();
+
+hpbeo::BaseElementObject(BaseElementObject&& other);
+
+hpbeo::BaseElementObject&
+hpbeo::operator=(BaseElementObject&& other);
+
+/* this should throw some error when the HTML is invalid. */
+hpbeo::BaseElementObject(const std::string& raw_html)
+	:
+
+hpbeo::BaseElementObject(const std::string& raw_html, BaseElementObjectPointer _parent);
+
+/* should allow access to attributes... */
+std::string&
+hpbeo::operator[](const std::string&);
+
+/* const access involves making*/
+const std::string&
+hpbeo::operator[](const std::string&) const;
+
+std::vector<hpbeo::BaseElementObjectPointer>&&
+hpbeo::css(const std::string& pattern) const;
