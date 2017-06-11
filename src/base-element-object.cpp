4945662e970b80dad933e6d3c3f4eaed037a3d4a
Charles <c650@users.noreply.github.com>
2017-06-11 13:08:47 -0400

    change namespace path compile error

--- src/base-element-object.cpp
+++ src/base-element-object.cpp
@@ -8,21 +8,20 @@
 #include <utility>
 
 namespace hp = HTMLParser;
-namespace hpbeo = hp::BaseElementObject;
 
-hpbeo::BaseElementObject();
-hpbeo::~BaseElementObject();
+hp::BaseElementObject::BaseElementObject();
+hp::BaseElementObject::~BaseElementObject();
 
 /* this should throw some error when the HTML is invalid. */
-hpbeo::BaseElementObject(const std::string& raw_html)
-	: hpbeo::BaseElementObject(raw_html, null) {}
+hp::BaseElementObject::BaseElementObject(const std::string& raw_html)
+	: hp::BaseElementObject::BaseElementObject(raw_html, null) {}
 
-hpbeo::BaseElementObject(const std::string& raw_html, BaseElementObjectPointer _parent)
+hp::BaseElementObject::BaseElementObject(const std::string& raw_html, BaseElementObjectPointer _parent)
 : parent(_parent) {
 	parse_raw_html(raw_html);
 }
 
-std::vector<hp::BaseElementObjectPointer>&& hpbeo::css(const std::string& pattern) const {
+std::vector<hp::BaseElementObjectPointer>&& hp::BaseElementObject::css(const std::string& pattern) const {
 	std::vector<BaseElementObjectPointer> results; /* all matches to pattern go here. */
 
 	// do all the stuff.
@@ -32,33 +31,33 @@ std::vector<hp::BaseElementObjectPointer>&& hpbeo::css(const std::string& patter
 
 /* should allow access to attributes... */
 std::string&
-hpbeo::operator[](const std::string& attr) {
+hp::BaseElementObject::operator[](const std::string& attr) {
 	return attributes[attr];
 }
 
 /* const access involves making*/
 const std::string&
-hpbeo::operator[](const std::string& attr) const  {
+hp::BaseElementObject::operator[](const std::string& attr) const  {
 	return attributes[attr];
 }
 
-void hpbeo::add_child(BaseElementObjectPointer child) {
+void hp::BaseElementObject::add_child(BaseElementObjectPointer child) {
 	if (child != nullptr) /* no sense in adding null children. */
 		children.push_back(child);
 }
 
-void hpbeo::set_parent(hp::BaseElementObjectPointer parent) {
+void hp::BaseElementObject::set_parent(hp::BaseElementObjectPointer parent) {
 	this->parent = parent;
 }
 
-const hp::BaseElementObjectPointer hpbeo::get_parent(void) const {
+const hp::BaseElementObjectPointer hp::BaseElementObject::get_parent(void) const {
 	return this->parent;
 }
 
-void hpbeo::set_tag_name(const std::string& name) {
+void hp::BaseElementObject::set_tag_name(const std::string& name) {
 	this->tag_name = name;
 }
 
-const std::string& hpbeo::get_tag_name(void) const {
+const std::string& hp::BaseElementObject::get_tag_name(void) const {
 	return this->tag_name;
 }
