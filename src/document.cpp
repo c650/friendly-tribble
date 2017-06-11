cd660809f8c900edcc96f6f573736586968ac279
Charles <c650@users.noreply.github.com>
2017-06-11 12:57:11 -0400

    moving code to new files

---
+++ src/document.cpp
@@ -0,0 +1,11 @@
+#include "./html.hpp"
+
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <stdexcept>
+#include <regex>
+#include <utility>
+
+namespace hp = HTMLParser;
+namespace hpdoc = hp::Document;
