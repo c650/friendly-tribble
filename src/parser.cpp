64b3934e15db1a90fdb0ed6945fac62e204d843d
Charles <c650@users.noreply.github.com>
2017-06-15 15:04:25 -0400

    working on parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -9,9 +9,15 @@
 
 namespace hp = HTMLParser;
 
-typedef std::queue<std::pair<size_t, std::smatch>> match_type;
+struct match_type {
+	size_t index;
+	std::smatch match;
+	bool is_beginning_tag;
+};
 
-static match_type do_regex_search(std::string s, const std::regex& pattern);
+static std::vector<match_type> do_regex_search(std::string s, const std::regex& pattern, bool is_beginning_tag);
+
+static std::vector<hp::BaseElementObjectPointer> process_tags(std::vector<match_type>::iterator begin, std::vector<match_type>::iterator end);
 
 /* incomplete garbage */
 hp::BaseElementObjectPointer hp::Document::parse_raw_html(const std::string& raw_html) {
@@ -19,8 +25,8 @@ hp::BaseElementObjectPointer hp::Document::parse_raw_html(const std::string& raw
 	const static std::regex   end_tag_regex("</([a-zA-Z]+[1-6]?)>");
 
 	/* this can be much better than it is now. */
-	match_type beginnings = do_regex_search(raw_html, start_tag_regex);
-	match_type ends       = do_regex_search(raw_html,   end_tag_regex);
+	std::vector<match_type> beginnings = do_regex_search(raw_html, start_tag_regex, true);
+	std::vector<match_type> ends       = do_regex_search(raw_html,  end_tag_regex, false);
 
 	if (beginnings.size() != ends.size()) {
 		throw html_parse_error("Open/closing tags unmatched.");
@@ -31,18 +37,25 @@ hp::BaseElementObjectPointer hp::Document::parse_raw_html(const std::string& raw
 		return nullptr;
 	}
 
-	size_t req = 1;
-	std::pair<size_t, std::smatch> first = beginnings.front();
-	beginnings.pop();
+	BaseElementObjectPointer element = nullptr;
+
+	std::vector<match_type> all = beginnings;
+	all.insert(all.end(), ends.begin(), ends.end());
+
+	std::sort(all.begin(), all.end(), [](const match_type& a, const match_type& b){return a.index < b.index;});
+
+
+
+	return element;
 }
 
-static match_type do_regex_search(std::string s, const std::regex& pattern) {
-	match_type results;
+static std::vector<match_type> do_regex_search(std::string s, const std::regex& pattern, bool is_beginning_tag) {
+	std::vector<match_type> results;
 
 	size_t offset = 0;
 	std::smatch match;
 	while (std::regex_search(s, match, pattern)) {
-		results.push(std::make_pair(offset + match.prefix().length(), match));
+		results.push_back({ offset + match.prefix().length(), match, is_beginning_tag });
 
 		offset += match.prefix().length() + match[0].length();
 
@@ -51,3 +64,43 @@ static match_type do_regex_search(std::string s, const std::regex& pattern) {
 
 	return results;
 };
+
+static std::vector<hp::BaseElementObjectPointer> process_tags(std::vector<match_type>::iterator begin, std::vector<match_type>::iterator end) {
+	std::vector<hp::BaseElementObjectPointer> top_level_elements;
+
+	auto start = begin;
+	auto finish = end-1;
+
+	int count = 0;
+
+	while (start != end) {
+		if (!start->is_beginning_tag) {
+			++start;
+			continue;
+		}
+		for (finish = start+1; finish != end; ++finish) {
+			if (!finish->is_beginning_tag) {
+				--count;
+			} else {
+				++count;
+			}
+			if (count <= 0)
+				break;
+		}
+
+		// do math for start to finish of entire element and stuff within
+		size_t length = finish->index - start->index - start->match[0].length();
+		top_level_elements.push_back(new hp::BaseElementObject(start->match[1], start->match.suffix().str().substr(0, length)));
+
+		// take care of element's children
+		for (hp::BaseElementObjectPointer elem : process_tags(start+1, finish))
+			top_level_elements.back()->add_child(elem);
+
+		// TODO: Read attributes.
+
+
+		start = finish + 1;
+	}
+
+	return top_level_elements;
+}
