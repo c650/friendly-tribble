324fb30a1b5cbaf2dcccab44eb57a68081b10367
Charles <c650@users.noreply.github.com>
2017-06-25 15:48:06 -0400

    add split function

--- src/include/helpers.hpp
+++ src/include/helpers.hpp
@@ -7,8 +7,11 @@
 
 #include <regex>
 
+#include <vector>
+#include <string>
+
 template<class BidirIt, class Func>
-static void general_regex_search(BidirIt first, BidirIt last, const std::regex& pattern, Func f) {
+void general_regex_search(BidirIt first, BidirIt last, const std::regex& pattern, Func f) {
 	std::smatch match;
 	size_t offset = 0;
 	while (std::regex_search(first + offset, last, match, pattern)) {
@@ -17,6 +20,18 @@ static void general_regex_search(BidirIt first, BidirIt last, const std::regex& 
 
 		offset += match.position() + match.length();
 	}
+}
+
+template <class BidirIt>
+std::vector<std::string> split_on_regex(BidirIt first, BidirIt, last, const std::regex& pattern) {
+	std::sregex_token_iterator start(first, last, pattern, -1);
+	std::sregex_token_iterator end;
+
+	std::vector<std::string> arr;
+	for (; start != end; ++start)
+		arr.push_back(*start);
+
+	return arr;
 }
 
 #endif
